b8015c29b82a2af22e1bf4d9e36d7a0fbe6f3e3c
Hari Acharya <136120359+HariAcharya80@users.noreply.github.com>
2023-11-11 22:20:42 +0545

    logout loading

--- src/components/LogoutDialog.tsx
+++ src/components/LogoutDialog.tsx
@@ -1,5 +1,7 @@
-import { Dispatch, SetStateAction, useContext } from "react";
+import { Dispatch, SetStateAction, useContext, useState } from "react";
+import ReactDOM from "react-dom";
 import { AuthContext } from "../hooks/AuthProvider";
+import LoadingDialog from "./LoadingDialog";
 
 interface LogoutDialogProps {
   showDialog: Dispatch<SetStateAction<boolean>>;
@@ -7,29 +9,39 @@ interface LogoutDialogProps {
 
 function LogoutDialog({ showDialog }: LogoutDialogProps) {
   const { logout } = useContext(AuthContext);
+  const [loading, setLoading] = useState(false);
   return (
-    <section className="absolute left-0 top-0 backdrop-blur-md w-screen h-screen flex justify-center">
-      <div className="bg-white shadow-2xl p-10 min-w-fit w-fit rounded mt-12 h-fit">
-        <h1 className="font-semibold text-2xl">Logout</h1>
-        <span>Are you sure you want to logout?</span>
-        <div className="w-full flex gap-5 mt-2">
-          <button
-            onClick={() => showDialog(false)}
-            className="transition-all bg-slate-500 hover:bg-slate-800 text-white rounded px-8 py-1"
-          >
-            Cancel
-          </button>
-          <button
-            onClick={async () => {
-              await logout();
-            }}
-            className="transition-all bg-rose-500 hover:bg-rose-800 text-white rounded px-8 py-1"
-          >
-            Logout
-          </button>
+    <>
+      {loading &&
+        ReactDOM.createPortal(
+          <LoadingDialog />,
+          document.getElementById("dialog") as HTMLElement
+        )}
+      <section className="absolute left-0 top-0 backdrop-blur-md w-screen h-screen flex justify-center">
+        <div className="bg-white shadow-2xl p-10 min-w-fit w-fit rounded mt-12 h-fit">
+          <h1 className="font-semibold text-2xl">Logout</h1>
+          <span>Are you sure you want to logout?</span>
+          <div className="w-full flex gap-5 mt-2">
+            <button
+              onClick={() => showDialog(false)}
+              className="transition-all bg-slate-500 hover:bg-slate-800 text-white rounded px-8 py-1"
+            >
+              Cancel
+            </button>
+            <button
+              onClick={async () => {
+                setLoading(true);
+                await logout();
+                return setLoading(false);
+              }}
+              className="transition-all bg-rose-500 hover:bg-rose-800 text-white rounded px-8 py-1"
+            >
+              Logout
+            </button>
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </>
   );
 }
 
