f1a720f35f602846260cbebe98e82d6c94fc1abf
Hari Acharya <136120359+HariAcharya80@users.noreply.github.com>
2023-11-11 19:09:51 +0545

    new layout

--- src/pages/Secure/DashboardLayout.tsx
+++ src/pages/Secure/DashboardLayout.tsx
@@ -1,20 +1,38 @@
-import React from "react";
+import React, { useContext } from "react";
 import Sidebar from "../../components/DashboardSidebar";
 import BottomMenu from "../../components/BottomMenu";
+import { AuthContext } from "../../hooks/AuthProvider";
+import { Navigate } from "react-router-dom";
 
 interface DashboardLayoutProps {
   children: React.ReactElement;
 }
 function DashboardLayout({ children }: DashboardLayoutProps) {
-  return (
-    <section className="absolute top-0 left-0 w-screen min-h-screen flex flex-col md:flex-row md:justify-start gap-4">
-      <Sidebar />
-      <section className="min-h-[90vh] md:h-full">{children}</section>
-      <section className="min-h-[10vh] md:hidden">
-        <BottomMenu />
-      </section>
-    </section>
-  );
+  const { user } = useContext(AuthContext);
+  if (user.signedIn) {
+    return (
+      <>
+        {/* for desktop */}
+        <section className="hidden left-0 top-0 w-screen h-screen md:flex gap-4 justify-center bg-slate-200">
+          <Sidebar />
+          <div className="w-[85vw] p-2 h-[100vh] overflow-y-scroll">
+            {children}
+          </div>
+        </section>
+
+        {/* for mobile */}
+        <section className="flex flex-col md:hidden min-h-screen max-h-screen h-screen">
+          <div className="absolute h-[90%] top-0 left-0 overflow-y-scroll p-2">
+            {children}
+          </div>
+          <BottomMenu />
+        </section>
+      </>
+    );
+  }
+  {
+    return <Navigate to={"/auth/login"} replace />;
+  }
 }
 
 export default DashboardLayout;
