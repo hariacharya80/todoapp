ca31dcbb9a1c5402db652250a0c163a0c5b77caf
Hari Acharya <136120359+HariAcharya80@users.noreply.github.com>
2023-11-11 15:44:34 +0545

    fixing mobile layout

--- src/pages/Secure/DashboardLayout.tsx
+++ src/pages/Secure/DashboardLayout.tsx
@@ -9,8 +9,10 @@ function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <section className="absolute top-0 left-0 w-screen min-h-screen flex flex-col md:flex-row md:justify-start gap-4">
       <Sidebar />
-      <>{children}</>
-      <BottomMenu />
+      <section className="min-h-[90vh] md:h-full">{children}</section>
+      <section className="min-h-[10vh] md:hidden">
+        <BottomMenu />
+      </section>
     </section>
   );
 }
