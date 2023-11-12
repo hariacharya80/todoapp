4efb3bbd99c4d46b6b3a87f3f0007da8b3ca957b
Hari Acharya <harry@hotelsynergy.net>
2023-11-13 01:15:56 +0545

    all commit

--- src/pages/Auth/Reset.tsx
+++ src/pages/Auth/Reset.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import LoadingDialog from "../../components/LoadingDialog";
-import ReactDOM from "react-dom";
 import UseBackend from "../../hooks/UseBackend";
+import toast from "react-hot-toast";
 
 function Reset() {
   const urlSearch = new URLSearchParams(document.location.search);
@@ -11,15 +11,59 @@ function Reset() {
   const [loading, setLoading] = useState(false);
   const { resetPassword } = UseBackend();
   const [email, setEmail] = useState("");
-  document.title = "Reset Password | MyTodoApp";
+  if (!token) {
+    document.title = "Reset Password | MyTodoApp";
+  } else {
+    document.title = "Change Password | MyTodoApp";
+  }
+
+  //state varibales for setting new password
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+
+  const [passwordValidation, setPasswordValidation] = useState({
+    err: false,
+    msg: "",
+  });
+
+  const [confirmPasswordValidation, setConfirmPasswordValidation] = useState({
+    err: false,
+    msg: "",
+  });
+
+  const validateNewPasswords = () => {
+    if (!newPassword) {
+      toast.error("New password can not be empty.");
+      return setPasswordValidation({
+        err: true,
+        msg: "New password can not be empty.",
+      });
+    }
+    if (newPassword.length < 8) {
+      toast.error("New password must be at least 8 characters.");
+      return setPasswordValidation({
+        err: true,
+        msg: "New password must be at least 8 characters long.",
+      });
+    }
+    if (newPassword !== confirmPassword) {
+      toast.error("Confirmation password do not match.");
+      setConfirmPasswordValidation({
+        err: true,
+        msg: "Confirmation password do not match.",
+      });
+    }
+  };
+
+  const handlePasswordChange = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    validateNewPasswords();
+  };
+
   return (
     <>
-      {loading &&
-        ReactDOM.createPortal(
-          <LoadingDialog />,
-          document.getElementById("dialog") as HTMLElement
-        )}
-      <section className="absolute min-w-2/3 top-0 left-0 flex justify-center w-screen min-h-screen bg-slate-200">
+      <section className="absolute top-0 left-0 flex justify-center w-screen min-h-screen bg-slate-200">
         {!token && (
           <form
             onSubmit={async (e) => {
@@ -28,7 +72,7 @@ function Reset() {
               await resetPassword(email);
               setLoading(false);
             }}
-            className="bg-white flex flex-col items-center h-fit p-12 mt-12"
+            className="bg-white w-1/3 min-w-fit rounded flex flex-col items-center h-fit p-12 mt-12"
           >
             <h1 className="font-bold text-3xl">Password Reset</h1>
             <p className="mb-2 text-slate-500">
@@ -44,15 +88,23 @@ function Reset() {
               }}
               className="w-full border-2 p-1 rounded"
               type="email"
+              disabled={loading}
               name="email"
               id="email"
               placeholder="jhon.sharma@gmail.com"
             />
             <button
               type="submit"
-              className="my-2 p-2 rounded bg-indigo-500 text-white w-full hover:bg-indigo-800 transition-colors"
+              className={
+                loading
+                  ? "flex justify-center cursor-not-allowed items-center gap-2 my-2 p-2 rounded bg-indigo-500 text-white w-full hover:bg-indigo-800 transition-colors"
+                  : "flex justify-center items-center gap-2 my-2 p-2 rounded bg-indigo-500 text-white w-full hover:bg-indigo-800 transition-colors"
+              }
             >
-              Send Reset Email
+              {loading && (
+                <div className="w-4 h-4 border-2 rounded-full border-black border-t-white animate-spin"></div>
+              )}
+              {loading ? "Please wait..." : "Send Reset Email"}
             </button>
             <a
               href="/auth/login"
@@ -68,37 +120,86 @@ function Reset() {
           </form>
         )}
         {token && (
-          <form className="bg-white rounded mt-12 items-center flex flex-col gap-2 h-fit p-12 w-1/3 min-w-fit">
+          <form
+            onSubmit={handlePasswordChange}
+            className="bg-white rounded mt-12 items-center flex flex-col gap-2 h-fit p-12 w-1/3 min-w-fit"
+          >
             <h1 className="font-bold text-3xl">Change Password</h1>
             <span className="text-slate-500 -mt-2">
               Please provide a new password for your account.
             </span>
-            <fieldset className="w-full">
-              <div className="w-full mb-1 mt-2 flex justify-start">
-                <label htmlFor="password">New password :</label>
-              </div>
-              <input
-                className="border-2 p-1 rounded w-full "
-                type="password"
-                name="password"
-                id="password"
-                placeholder="********"
-              />
-            </fieldset>
-            <fieldset className="w-full">
-              <div className="w-full flex justify-start">
-                <label htmlFor="confirm-password">Confirm Password :</label>
-              </div>
-              <input
-                className="border-2 p-1 rounded w-full"
-                placeholder="********"
-                type="password"
-                name="confirm-password"
-                id="confirm-password"
-              />
-            </fieldset>
-            <button className="bg-indigo-500 py-2 rounded w-full text-white hover:bg-indigo-800 transition-colors mt-3">
-              Set New Password
+
+            <div className="w-full -mb-2 flex justify-start">
+              <label htmlFor="password">New password :</label>
+            </div>
+            <input
+              className={
+                passwordValidation.err
+                  ? "border-2 p-1 rounded w-full border-rose-500 outline-rose-500"
+                  : "border-2 p-1 rounded w-full "
+              }
+              type="password"
+              name="password"
+              value={newPassword}
+              onChange={(e) => {
+                setPasswordValidation({
+                  err: false,
+                  msg: "",
+                });
+                setConfirmPasswordValidation({
+                  err: false,
+                  msg: "",
+                });
+                setNewPassword(e.target.value);
+              }}
+              disabled={loading}
+              id="password"
+              placeholder="********"
+            />
+            {passwordValidation.err && (
+              <span className="text-rose-500 w-full -mt-2">
+                {passwordValidation.msg}
+              </span>
+            )}
+            <div className="w-full flex justify-start">
+              <label htmlFor="confirm-password">Confirm Password :</label>
+            </div>
+            <input
+              className={
+                confirmPasswordValidation.err
+                  ? "border-2 p-1 rounded w-full outline-rose-500 border-rose-500"
+                  : "border-2 p-1 rounded w-full"
+              }
+              placeholder="********"
+              value={confirmPassword}
+              onChange={(e) => {
+                setConfirmPasswordValidation({
+                  err: false,
+                  msg: "",
+                });
+                setConfirmPassword(e.target.value);
+              }}
+              disabled={loading}
+              type="password"
+              name="confirm-password"
+              id="confirm-password"
+            />
+            {confirmPasswordValidation.err && (
+              <span className="text-rose-500 w-full -mt-2">
+                {confirmPasswordValidation.msg}
+              </span>
+            )}
+            <button
+              className={
+                loading
+                  ? "flex justify-center cursor-not-allowed items-center gap-2 bg-indigo-500 py-2 rounded w-full text-white hover:bg-indigo-800 transition-colors mt-3"
+                  : "flex justify-center items-center gap-2 bg-indigo-500 py-2 rounded w-full text-white hover:bg-indigo-800 transition-colors mt-3"
+              }
+            >
+              {loading && (
+                <div className="w-4 h-4 rounded-full animate-spin border-2 border-black border-t-white"></div>
+              )}
+              {loading ? "Please wait..." : "Set New Password"}
             </button>
             <span className="text-slate-500">
               Back to{" "}
