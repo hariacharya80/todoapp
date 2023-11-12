1b8b0cfb32108b9a4658efce276d6eeb8837f332
Hari Acharya <harry@hotelsynergy.net>
2023-11-13 03:36:58 +0545

    todo design/ loading screen /empty

--- src/pages/Secure/Todo.tsx
+++ src/pages/Secure/Todo.tsx
@@ -14,6 +14,7 @@ interface todoItemTypes {
 }
 
 function Todo() {
+  document.title = "Todo | MyTodoApp";
   const [todoList, setTodoList] = useState<todoItemTypes[]>([]);
   const [showAddTodoList, setShowAddTodoList] = useState(false);
   const [fetching, setFetching] = useState(true);
@@ -50,14 +51,52 @@ function Todo() {
           document.getElementById("dialog") as HTMLElement
         )}
       <section>
-        <h1>Todo List</h1>
-        <button
-          onClick={() => setShowAddTodoList((prev) => !prev)}
-          className="bg-slate-100 hover:bg-slate-300 transition-colors px-8 py-1 rounded"
-        >
-          Add New Todo
-        </button>
-        {fetching && <span>Please wait, the data is being loaded......</span>}
+        <div className="w-full flex justify-between items-center">
+          <div className="flex flex-col gap-0">
+            <h1 className="font-bold text-3xl my-2">Todo</h1>
+            <span className="-mt-3 text-slate-500">
+              Divide and rule your tasks!
+            </span>
+          </div>
+          <button
+            onClick={() => setShowAddTodoList((prev) => !prev)}
+            className="bg-indigo-500 text-white hover:text-black hover:bg-slate-300 transition-colors px-8 py-1 rounded"
+          >
+            Add New Todo
+          </button>
+        </div>
+        {fetching && (
+          <div className="w-full flex justify-center items-center h-[50vh]">
+            <div className="flex flex-col items-center">
+              <div className="w-8 h-8 border-2 border-black border-t-indigo-500 rounded-full animate-spin"></div>
+              <h1 className="font-bold text-xl  my-2">Loading todos...</h1>
+              <div className="flex flex-col items-center bg-slate-300 mt-12 p-2 rounded-xl">
+                <span>Did you know: </span>
+                <span>
+                  Those who break there task in small todo are 10x more
+                  productive.
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+        {todoList.length < 1 && !fetching && (
+          <div className="flex w-full justify-center my-2 py-[10vh]">
+            <div className="flex flex-col gap-2 items-center">
+              <img src="/images/empty.png" alt="No items found." />
+              <h1 className="font-semibold text-3xl">Nothing to Worry.</h1>
+              <p className="text-slate-400">
+                Seems like you don't have added any tasks here.
+              </p>
+              <button
+                onClick={() => setShowAddTodoList(true)}
+                className="bg-indigo-500 w-1/2 px-8 py-1 rounded text-white hover:bg-indigo-700 transition-colors"
+              >
+                Add new task
+              </button>
+            </div>
+          </div>
+        )}
         {!fetching && (
           <>
             <div className="w-full grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 lg:grid-cols-4 gap-x-4">
